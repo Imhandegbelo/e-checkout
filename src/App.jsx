199f45f27c873474f6e6da6df846fb4ce477e3f1
Imhandegbelo <abramgeo5@gmail.com>
2023-10-15 19:42:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-stone-900'>
+      <Navbar />djsf
+    </div>
+  )
+}
+
+export default App
